18c50f5d25b86d1c3777a8647ecfcabfe25f1e0c
icodragon <icodragon7@gmail.com>
2024-09-07 11:06:57 -0300

    Added fourth task

---
+++ tasks/task4/checker_task4.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+GREEN=$'\e[32m'
+RED=$'\e[31m'
+YELLOW=$'\e[33m'
+RESET=$'\e[0m'
+
+DONE="[done]"
+BUSY="[busy]"
+FAILED="[failed]"
+
+main() {
+echo "
+  ___ _____ ___  _____   __
+ / __|_   _/ _ \| _ \ \ / /
+ \__ \ | || (_) |   /\ V /
+ |___/ |_| \___/|_|_\ |_|
+
+created checker validator team @stakeme
+"
+
+    echo "> Please enter your moniker validator"
+    read -r MONIKER
+
+    echo "> Please enter your signature"
+    read -r SIGNATURE
+
+    echo "> Please enter your wallet Cosmos address"
+    read -r COSMOS_WALLET
+
+    echo "> Please enter your wallet EVM address"
+    read -r EVM_WALLET
+
+    checked_timestamp=$(date +%s)
+    echo "Paste the content into data.json
+    {
+      \"moniker\": \"$MONIKER\",
+      \"signature\": \"$SIGNATURE\",
+      \"cosmos_wallet\": \"$COSMOS_WALLET\",
+      \"evm_wallet\": \"$EVM_WALLET\",
+      \"timestamp\": $checked_timestamp
+    }
+    "
+}
+
+main
